bb4cb686194a8525d6e77e9d45b589cca4db5621
serpuntij <denissolovev3651@gmail.com>
2021-09-07 12:55:30 -0700

    add-dotenv

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,12 +2,12 @@ import supertest from 'supertest';
 import {expect} from 'chai';
 
 describe('auth', function (){
-    const request = supertest('https://paysis.herokuapp.com');
+    const request = supertest(process.env.BASE_URL);
 
     it('successful log in', function (){
         request
             .post('/auth')
-            .send({login: 'adminius', password: 'supers3cret'})
+            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
             .end(function (err, res){
                expect(res.statusCode).to.eq(200);
                expect(res.body.token).not.to.be.undefined;
