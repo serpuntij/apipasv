25b55e011243c0d023518a3f4d41b51279fab9a9
serpuntij <denissolovev3651@gmail.com>
2021-09-07 15:52:10 -0700

    add hooks 2

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,45 +1,44 @@
 import supertest from 'supertest';
-import {expect} from 'chai';
+import { expect } from 'chai';
 
-describe('auth', function (){
-    const request = supertest(process.env.BASE_URL);
+describe('auth', function() {
     let result;
 
-    describe('successful log in', function(){
-        before(function (){
-            result = supertest(process.env.BASE_URL)
+    describe('successful log in', function() {
+        before(async function() {
+            await supertest(process.env.BASE_URL)
                 .post('/auth')
-                .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+                .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
+                .then(res => {
+                    result = res;
+                });
         });
 
-
-        it('response status code is 200', function (){
-            result.expect(200);
+        it('response status code is 200', function() {
+            expect(result.statusCode).to.eq(200);
         });
 
-        it('response body contains authorization token', function(){
-            result.end(function(err, res){
-                expect(res.body.token).not.to.be.undefined;
-            });
+        it('response body contains authorization token', function() {
+            expect(result.body.token).not.to.be.undefined;
         });
     });
 
-    describe('login withwrong credentials should return error', function () {
-
-        before(function (){
-            result = supertest(process.env.BASE_URL)
+    describe('log in with wrong credentials should return error', function() {
+        before(async function() {
+            await supertest(process.env.BASE_URL)
                 .post('/auth')
-                .send({login: 'wrong', password: 'wrong'})
-        });
-
-        it('response status code is 404', function () {
-            result.expect(404);
-        });
-
-        it('response status code is 404', function () {
-            result.end(function (err, res){
-                    expect(res.body.message).to.eq('Wrong login or password.');
+                .send({ login: 'wrong', password: 'wrong' })
+                .then(res => {
+                    result = res;
                 });
+        });
+
+        it('response status code is 404', function() {
+            expect(result.statusCode).to.eq(404);
+        });
+
+        it('response status code is 404', function() {
+            expect(result.body.message).to.eq('Wrong login or password.');
         });
     });
 });
